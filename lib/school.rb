445caa4ff14d7157743d8cd2b46efa411c8a84bc
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-03 17:04:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,27 @@
-school.add_student("AC Slater", 9)
-school.add_student("Kelly Kapowski", 10)
-school.add_student("Screech", 11)
-school.roster
+require 'pry'
+class School
+
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |grade, students|
+      students.sort!
+    end
+    roster.sort.to_h
+  end
+end 
